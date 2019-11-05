8bc7dbd0a7aa327017d1d74c3999c4fd09821c1f
PondJaTH <palapon2545@gmail.com>
2019-11-05 13:04:32 +0700

    footer fix

--- load.php
+++ load.php
@@ -216,43 +216,63 @@
     <br>
     <br>
     <hr>
-    <div class="col-md-4">
+    <footer style="background-color: rgba(0, 0 , 0, 0.8);">
+        <div class="container">
+            <hr>
+            <div class="row">
+                <div class="col-12">
+                    <h4 style="color: white">โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)</h4>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
                     <p style="color: white">เกี่ยวกับ</p>
                     <ul>
-                        <li><a href="../load.php?img=http://smd-s.kku.ac.th/home/images/smd-55/data09.png">ประวัติโรงเรียน</a></li>
-                        <li><a href="../load.php?img=http://smd-s.kku.ac.th/home/images/smd-55/data010.png">ปรัชญา</a></li>
-                        <li><a href="../load.php?img=http://smd-s.kku.ac.th/home/images/smd-55/data08.png">วิสัยทัศน์ พันธกิจ</a></li>
-                        <li><a href="../load.php?img=http://smd-s.kku.ac.th/home/images/smd-55/data06.png">เป้าหมายเชิงกลยุทธ์</a></li>
-                        <li><a href="../load.php?img=http://smd-s.kku.ac.th/home/images/smd-55/data07.png">คุณลักษณะอันพึงประสงค์</a></li>
-                        <li><a href="../load.php?img=http://smd-s.kku.ac.th/home/images/smd-55/data12.png">คณะกรรมการประจำโรงเรียน</a></li>
-                        <li><a href="../load.php?img=http://smd-s.kku.ac.th/home/images/smd-58/managementstructure57.jpg">โครงสร้างการบริหาร</a></li>
-                        <li><a href="#">ทำเนียบผู้บริหาร</a></li>
-                        <li><a href="#">คณะผู้บริหาร</a></li>
-                        <li><a href="#">บุคลากร</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">ประวัติโรงเรียน</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">ปรัชญา</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">เป้าหมายเชิงกลยุทธ์</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">คุณลักษณะอันพึงประสงค์</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">คณะกรรมการประจำโรงเรียน</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">โครงสร้างการบริหาร</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">ทำเนียบผู้บริหาร</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">คณะผู้บริหาร</a></li>
+                        <li style="color: #ff6c00"><a href="#" style="color: white">บุคลากร</a></li>
                     </ul>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <p style="color: white">หน่วยงาน</p>
                     <ul>
-                        <li><a style="color: white" href="#"> งานแผนและประกันคุณภาพ </a></li>
-                        <li><a style="color: white" href="#"> งานแนะแนว </a></li>
-                        <li><a style="color: white" href="#"> งานทะเบียน </a></li>
-                        <li><a style="color: white" href="#"> งานพัฒนาบุคลิกภาพ </a></li>
-                        <li><a style="color: white" href="#"> งานห้องสมุด </a></li>
-                        <li><a style="color: white" href="#"> ชมรมผู้ปกครองและครู </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="#"> งานแผนและประกันคุณภาพ </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="#"> งานแนะแนว </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="#"> งานทะเบียน </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="#"> งานพัฒนาบุคลิกภาพ </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="#"> งานห้องสมุด </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="#"> ชมรมผู้ปกครองและครู </a></li>
                     </ul>
 
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <p style="color: white">ปฏิทิน</p>
                     <ul>
-                        <li><a style="color: white" href="#"> ปฏิทินโรงเรียน </a></li>
-                        <li><a style="color: white" href="../calendar.html"> ตารางเรียน </a></li>
-                        <li><a style="color: white" href="#"> ตารางสอบ </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="#"> ปฏิทินโรงเรียน </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="calendar.html"> ตารางเรียน </a></li>
+                        <li style="color: #ff6c00"><a style="color: white" href="#"> ตารางสอบ </a></li>
                     </ul>
-                    <a style="color: white" href="../forum.html"> SMD Forum </a></li>
+                    <a style="color: white" href="forum.html"> SMD Forum </a></li>
                     <br><a style="color: white" href="#"> SMD Shop </a></li>
                 </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-12">
+                    <center>
+                        <p style="color: white">Copyright 2019 © MyWebsite. By PondJa & Risaka .</center>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-3.3.1.min.js"></script>
 
