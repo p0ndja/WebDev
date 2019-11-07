5f253f2a8464e23cbf9fdb2f45663f03e6be112a
PondJaTH <palapon2545@gmail.com>
2019-11-07 23:19:17 +0700

    Global Footer

---
+++ global/footer.php
@@ -0,0 +1,57 @@
+<footer style="background-color: rgba(0, 0 , 0, 0.8);">
+            <div class="container">
+                <hr>
+                <div class="row">
+                    <div class="col-12">
+                        <h4 style="color: white">โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)</h4>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        <p style="color: white">เกี่ยวกับ</p>
+                        <ul>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">ประวัติโรงเรียน</a></li>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">ปรัชญา</a></li>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">เป้าหมายเชิงกลยุทธ์</a></li>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">คุณลักษณะอันพึงประสงค์</a></li>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">คณะกรรมการประจำโรงเรียน</a></li>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">โครงสร้างการบริหาร</a></li>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">ทำเนียบผู้บริหาร</a></li>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">คณะผู้บริหาร</a></li>
+                            <li style="color: #ff6c00"><a href="#" style="color: white">บุคลากร</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <p style="color: white">หน่วยงาน</p>
+                        <ul>
+                            <li style="color: #ff6c00"><a style="color: white" href="#"> งานแผนและประกันคุณภาพ </a></li>
+                            <li style="color: #ff6c00"><a style="color: white" href="#"> งานแนะแนว </a></li>
+                            <li style="color: #ff6c00"><a style="color: white" href="#"> งานทะเบียน </a></li>
+                            <li style="color: #ff6c00"><a style="color: white" href="#"> งานพัฒนาบุคลิกภาพ </a></li>
+                            <li style="color: #ff6c00"><a style="color: white" href="#"> งานห้องสมุด </a></li>
+                            <li style="color: #ff6c00"><a style="color: white" href="#"> ชมรมผู้ปกครองและครู </a></li>
+                        </ul>
+
+                    </div>
+                    <div class="col-md-3">
+                        <p style="color: white">ปฏิทิน</p>
+                        <ul>
+                            <li style="color: #ff6c00"><a style="color: white" href="#"> ปฏิทินโรงเรียน </a></li>
+                            <li style="color: #ff6c00"><a style="color: white" href="calendar.html"> ตารางเรียน </a></li>
+                            <li style="color: #ff6c00"><a style="color: white" href="#"> ตารางสอบ </a></li>
+                        </ul>
+                        <a style="color: white" href="forum.html"> SMD Forum </a></li>
+                        <br><a style="color: white" href="#"> SMD Shop </a></li>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-12">
+                        <center>
+                            <p style="color: white">Copyright 2019 © MyWebsite. By PondJa & Risaka .</center>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
