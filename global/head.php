839751f61e83db2ac0408a219c767bf1fa04e097
PondJaTH <palapon2545@gmail.com>
2019-12-10 21:32:14 +0700

    clean up

--- global/head.php
+++ global/head.php
@@ -1,61 +1,34 @@
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- KANIT FONT -->
-    <link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
+<!-- KANIT FONT -->
+<link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
 
-    <!-- Must be same here -->
-    <link rel="shortcut icon" href="../global/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="../global/favicon.ico" type="image/x-icon">
-    <title>โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)</title>
-    <link rel="image_src" href="https://tcas.pondja.com/smdlogo.png" />
-    <meta property="og:image" content="https://tcas.pondja.com/smdlogo.png" />
-    <meta property="og:title" content="โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)" />
-    <meta property="og:description"
-        content="123 มหาวิทยาลัยขอนแก่น โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ถนนมิตรภาพ ตำบลในเมือง อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40002 โทรศัพท์ / โทรสาร 043202044" />
+<link rel="shortcut icon" href="../global/favicon.ico" type="image/x-icon">
+<link rel="icon" href="../global/favicon.ico" type="image/x-icon">
+<title>โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)</title>
+<link rel="image_src" href="https://tcas.pondja.com/smdlogo.png" />
+<meta property="og:image" content="https://tcas.pondja.com/smdlogo.png" />
+<meta property="og:title" content="โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)" />
+<meta property="og:description" content="123 มหาวิทยาลัยขอนแก่น โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ถนนมิตรภาพ ตำบลในเมือง อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40002 โทรศัพท์ / โทรสาร 043202044" />
 
-    <!-- Bootstrap -->
-    <link href="../css/bootstrap-4.3.1.css" rel="stylesheet">
-    <!-- For Callout -->
-    <link href="https://v4-alpha.getbootstrap.com/assets/css/docs.min.css" rel="stylesheet">
-    <!-- Icon :: See More at https://useiconic.com/open/ -->
-    <link href="../icon/font/css/open-iconic.css" rel="stylesheet">
-    <!-- Icon :: See More at https://fontawesome.com/icons?d=gallery -->
-    <script src="https://kit.fontawesome.com/7454809de8.js" crossorigin="anonymous"></script>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-    <!-- Material Design Bootstrap -->
-    <link href="../css/mdb.min.css" rel="stylesheet">
-    <!-- Your custom styles (optional) -->
-    <link href="../css/style.css" rel="stylesheet">
-    <!---->
+<link href="../css/mdb.min.css" rel="stylesheet"> <!--MDBootstrap need to be higher than Bootstrap-->
+<link href="../css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link href="https://v4-alpha.getbootstrap.com/assets/css/docs.min.css" rel="stylesheet">
 
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"
-        id="bootstrap-css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
-    <!------ Include the above in your HEAD tag ---------->
-    <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+<!-- Icon :: See More at https://useiconic.com/open/ -->
+<link href="../icon/font/css/open-iconic.css" rel="stylesheet">
+<!-- Icon :: See More at https://fontawesome.com/icons?d=gallery -->
+<script src="https://kit.fontawesome.com/7454809de8.js" crossorigin="anonymous"></script>
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="../js/jquery-3.3.1.min.js"></script>
+<script src="../js/mdb.min.js"></script>
 
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="../js/popper.min.js"></script>
-    <script src="../js/bootstrap-4.3.1.js"></script>
-
-    <!-- SCRIPT THAT ADD FROM MDBootstrap-->
-    <!-- SCRIPTS -->
-    <!-- JQuery -->
-    <script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <!--<script type="text/javascript" src="../js/bootstrap.min.js"></script>
-    <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="../js/mdb.min.js"></script>
-    <!-- END HERE -->
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.js"></script>
