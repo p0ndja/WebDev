ca07c96708ed1bb20206da9c5c9ee2f8923baaf8
PondJaTH <palapon2545@gmail.com>
2019-12-10 21:35:25 +0700

    mdb exterminate

---
+++ global/config.php
@@ -0,0 +1,3 @@
+<?php
+    $use_mdbootstrap = true;
+?>
--- global/head.php
+++ global/head.php
@@ -13,7 +13,13 @@
 <meta property="og:title" content="โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)" />
 <meta property="og:description" content="123 มหาวิทยาลัยขอนแก่น โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ถนนมิตรภาพ ตำบลในเมือง อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40002 โทรศัพท์ / โทรสาร 043202044" />
 
+<?php include 'config.php';
+if ($use_mdbootstrap) { ?>
 <link href="../css/mdb.min.css" rel="stylesheet"> <!--MDBootstrap need to be higher than Bootstrap-->
+<script src="../js/mdb.min.js"></script>
+<?php }
+?>
+
 <link href="../css/style.css" rel="stylesheet">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link href="https://v4-alpha.getbootstrap.com/assets/css/docs.min.css" rel="stylesheet">
@@ -30,5 +36,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
 
-<script src="../js/mdb.min.js"></script>
 
