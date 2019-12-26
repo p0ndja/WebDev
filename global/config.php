5c1827bc9de8410e580231bc5d1aa1aeb1413a20
PondJaTH <palapon2545@gmail.com>
2019-12-27 00:03:40 +0700

    Update config.php

--- global/config.php
+++ global/config.php
@@ -1,5 +1,5 @@
 <?php
     $use_mdbootstrap = true;
     $snow_effect = true; //If enable this, Script for loading modal will bug.
-    $patch = "ปรับปรุงล่าสุด: 25 ธันวาคม 2019 เวลา 14.41 น.";
+    $patch = "ปรับปรุงล่าสุด: 26 ธันวาคม 2019 เวลา 22.58 น.";
 ?>
