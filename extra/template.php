899f8d4812907ec2ff668858a3fd879f351fbb1f
PondJaTH <palapon2545@gmail.com>
2019-12-27 16:33:27 +0700

    Create template.php

---
+++ extra/template.php
@@ -0,0 +1,23 @@
+<?php include '../global/connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../global/head.php';?>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-normal fixed-top scrolling-navbar" id="nav"
+        role="navigation">
+        <?php include '../global/navbar.php'; ?>
+    </nav>
+    <div class="container" id="container" style="padding-top: 88px">
+
+    </div>
+</body>
+
+<?php include '../global/footer.php' ?>
+<?php include '../global/popup.php' ?>
+
+</html>
