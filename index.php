796fa49ffacb1e5b73da5dab61c1108bad8938ca
PondJaTH <palapon2545@gmail.com>
2019-12-29 14:35:01 +0700

    update landing_page

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <html>
 
 <head>
-    <meta http-equiv="refresh" content="0; url=./home/" />
+    <meta http-equiv="refresh" content="1; url=./home/" />
     <meta charset="UTF-8">
     <?php include './global/head.php'; ?>
+    <style>
+        .sooperIdea {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
 </head>
+
+<body>
+    <div class="container">
+        <img src="./assets/images/landing_page.gif" class="sooperIdea">
+    </div>
+</body>
 
 </html>
