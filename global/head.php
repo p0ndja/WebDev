2a854535de8c8c812cfdf76f5b5fd969bf8c8594
PondJaTH <palapon2545@gmail.com>
2019-12-29 14:34:39 +0700

    Update head.php

--- global/head.php
+++ global/head.php
@@ -5,39 +5,39 @@
 <!-- KANIT FONT -->
 <link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
 
-<link rel="shortcut icon" href="../global/favicon.ico" type="image/x-icon">
-<link rel="icon" href="../global/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="../assets/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon">
 <title>โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)</title>
 <link rel="image_src" href="https://tcas.pondja.com/smdlogo.png" />
 <meta property="og:image" content="https://tcas.pondja.com/smdlogo.png" />
 <meta property="og:title" content="โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)" />
 <meta property="og:description" content="123 มหาวิทยาลัยขอนแก่น โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ถนนมิตรภาพ ตำบลในเมือง อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40002 โทรศัพท์ / โทรสาร 043202044" />
 
-<link rel="stylesheet" href="../global/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 <?php include 'config.php';
 if ($use_mdbootstrap) { ?>
-<link href="../global/css/mdb.min.css" rel="stylesheet"> <!--MDBootstrap need to be higher than Bootstrap-->
+<link href="../assets/css/mdb.min.css" rel="stylesheet"> <!--MDBootstrap need to be higher than Bootstrap-->
 <?php }
 ?>
 
-<link href="../global/css/style.css" rel="stylesheet">
+<link href="../assets/css/style.css" rel="stylesheet">
 
 <!-- Icon :: See More at https://fontawesome.com/icons?d=gallery -->
 <script src="https://kit.fontawesome.com/7454809de8.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 <!-- jQuery -->
-<script type="text/javascript" src="../global/js/jquery.min.js"></script>
+<script type="text/javascript" src="../assets/js/jquery.min.js"></script>
 <!-- Bootstrap tooltips -->
-<script type="text/javascript" src="../global/js/popper.min.js"></script>
+<script type="text/javascript" src="../assets/js/popper.min.js"></script>
 <!-- Bootstrap core JavaScript -->
-<script type="text/javascript" src="../global/js/bootstrap.min.js"
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"
     integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
 </script>
 <!-- MDB core JavaScript -->
-<script type="text/javascript" src="../global/js/mdb.min.js"></script>
+<script type="text/javascript" src="../assets/js/mdb.min.js"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css" rel="stylesheet">
-<script src="../global/js/summernote-bs4.js"></script>
+<script src="../assets/js/summernote-bs4.js"></script>
 
 
 
