a6bda9fc7a05053cb5adb28346bb10594942d8e4
PondJaTH <palapon2545@gmail.com>
2020-01-17 19:45:01 +0700

    Create extra/close.php

---
+++ extra/close.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="th">
+
+<head>
+    <?php include '../global/head.php'; ?>
+    <style>
+        .center {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+
+<body class="admin">
+    <div class="container" id="container" style="padding-top: 88px">
+        <div class="center">
+                <div class="text-center text-white">
+                    <h3>ปิดปรับปรุงชั่วคราว</h3>
+                    <h1>(╯°□°）╯︵ ┻━┻</h1>
+                    <img src="https://i.imgur.com/okHmU2N.gif" width=400>
+                </div>
+            
+        </div> 
+    </div>
+</body>
+</html>
