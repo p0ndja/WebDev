55ae796f4712b4ad9fc79fee87a0b51343ff2007
PondJaTH <palapon2545@gmail.com>
2020-01-26 11:16:08 +0700

    Update profile/save.php
    Replace JPG/PNG to Base64

--- profile/save.php
+++ profile/save.php
@@ -1,60 +1,31 @@
 <?php 
+    include '../global/base64.php';
     include '../global/connect.php';
 
     if (isset($_POST['edit_submit'])) {
         $id = $_SESSION['id'];
         $query = "SELECT * FROM `profile` WHERE id = '$id'";
         $result = mysqli_query($conn, $query);
-        if (! $result) {
-            die('Could not get data: ' . mysqli_error($conn));
-        }
+        if (! $result) die('Could not get data: ' . mysqli_error($conn));
 
         $user = $_SESSION['user'];
         $text = $_POST['text'];
 
         $re = mysqli_query($conn, "UPDATE `profile` set greetings = '$text' WHERE id = $id");
-        if (! $re) {
-            die('Could not update text: ' . mysqli_error($conn));
-        }
-
-        if(isset($_FILES['profile_upload']) && $_FILES['profile_upload']['name'] != ""){
-            $name_file =  $_FILES['profile_upload']['name'];
-            $tmp_name =  $_FILES['profile_upload']['tmp_name'];
-
-            date_default_timezone_set('Asia/Bangkok'); $date = date('YmdHis', time());
+        if (! $re) die('Could not update text: ' . mysqli_error($conn));
+        
             
-            $locate_img ="../profile/images/";
-            move_uploaded_file($tmp_name,$locate_img.$name_file);
-
-            rename($locate_img.$name_file, $locate_img.$user.'_'.$date.'_'.$name_file);
-
-            $finaldir = $locate_img.$user.'_'.$date.'_'.$name_file;
-
-            $_SESSION['pi'] = $finaldir;
-
-            $r = mysqli_query($conn, "UPDATE `profile` SET profile = '$finaldir' WHERE id = $id");
-            if (! $r) {
-                die("Could not set profile: " . mysqli_error($conn));
-            }
+        if(isset($_FILES['profile_upload']) && $_FILES['profile_upload']['name'] != ""){
+            $base64 = base64($_FILES['profile_upload'], $user, 'image');
+            $_SESSION['pi'] = $base64;
+            $r = mysqli_query($conn, "UPDATE `profile` SET profile = '$base64' WHERE id = $id");
+            if (! $r) die("Could not set profile: " . mysqli_error($conn));
         }
 
         if(isset($_FILES['background_upload']) && $_FILES['background_upload']['name'] != ""){
-            $name_file =  $_FILES['background_upload']['name'];
-            $tmp_name =  $_FILES['background_upload']['tmp_name'];
-
-            date_default_timezone_set('Asia/Bangkok'); $date = date('Y-m-d_H-i-s', time());
-            
-            $locate_img ="../cache/";
-            move_uploaded_file($tmp_name,$locate_img.$name_file);
-
-            rename($locate_img.$name_file, $locate_img.$user.'_'.$date.'_'.$name_file);
-
-            $finaldir = $locate_img.$user.'_'.$date.'_'.$name_file;
-
-            $r = mysqli_query($conn, "UPDATE `profile` SET background = '$finaldir' WHERE id = $id");
-            if (! $r) {
-                die("Could not set profile: " . mysqli_error($conn));
-            }
+            $base64 = base64($_FILES['background_upload'], $user, 'image');
+            $r = mysqli_query($conn, "UPDATE `profile` SET background = '$base64' WHERE id = $id");
+            if (! $r) die("Could not set profile_background: " . mysqli_error($conn));
         }
     }
     header("Location: ../profile");
