a5bc9c96f24fde51930d47213fb797c2bba58207
PondJaTH <palapon2545@gmail.com>
2020-01-27 18:18:12 +0700

    Create about.php

---
+++ extra/about.php
@@ -0,0 +1,106 @@
+<?php include '../global/connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../global/head.php';?>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-normal fixed-top scrolling-navbar" id="nav"
+        role="navigation">
+        <?php include '../global/navbar.php'; ?>
+    </nav>
+    <div class="container mb-4" id="container" style="padding-top: 88px">
+        <p>
+            <h1 class="font-weight-bold">WebDev</h1>
+            <h5>
+            <hr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            สำหรับโปรเจค WebDev นี้เป็นโปรเจคที่พัฒนาขึ้น บนภาษา HTML, PHP, CSS, JavaScript, SQL (รวมถึง Bootstrap
+            4.4.1) เพื่อสร้างเว็บไซต์โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)
+            ตามปณิธานของพวกเราที่ตั้งใจเอาไว้ว่าจะให้มันเป็นโปรเจคจบของพวกเรา PondJaᵀᴴ และ ˢᵖᵉᶜᵗᵉʳRisaka
+            ซึ่งโปรเจคนี้ถือเป็นโปรเจคด้านเว็บไซต์แรกที่มีโอกาสได้ทำ ซึ่งถ้าหากมีข้อผิดพลาดหรือบัคใด ๆ ที่รู้สึกขัดใจ
+            สามารถ Plil เข้ามาได้เลยนะครับ จะได้ทำการปรับแก้ไขให้
+
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This 'WebDev' project is base on HTML, PHP, CSS,
+            JavaScript, SQL Computer languages (with Bootstrap 4.4.1) to create our dream website for our school, The
+            demonstration school of Khon Kaen University (Mo Din Daeng) This is our first website project, if there's
+            something which is not prefectly. Feel free to create a plil request to us.
+
+            <br><br>
+            <p class="font-weight-bold">This project has a 'GNU General Public License v3.0', please keep in mind that
+                you'll need to opensource your edited code if you using our code.</p>
+                <h5>
+        </p>
+        <div class="row mb-3">
+            <div class="col-12 col-md-4">
+                <img class="img-fluid mb-1" src="https://me.pondja.com/img/Annotation%202019-11-29%20114539.jpg">
+            </div>
+            <div class="col-12 col-md-4">
+                <img class="img-fluid mb-1" src="https://me.pondja.com/img/Annotation%202019-11-29%20115148.jpg">
+            </div>
+            <div class="col-12 col-md-4">
+                <img class="img-fluid mb-1" src="https://me.pondja.com/img/Annotation%202019-11-29%20115227.jpg">
+            </div>
+        </div>
+        <p>
+            <h2 class="font-weight-bold">Contributor</h2>
+            <hr>
+            <h5 class="font-weight-bold">Developer</h5>
+            <ul>
+            <li>Palapon Soontornpas - Project Director: Full-stack Developer <a href="https://www.facebook.com/p0ndja" target="_blank"> <i class="fas fa-external-link-alt"></i></a></li>
+            <li>Sahatsawat Sudaphonrat - Art Director + Webpage Editor <a href="https://www.facebook.com/Sahatsawat.Risaka" target="_blank"> <i class="fas fa-external-link-alt"></i></a></li>
+            </ul>
+            <h5 class="font-weight-bold">Consultant</h5>
+            <ul>
+            <li>Asst. Prof. Donchai Srisamran | Associate School Director of The demonstration school of Khon Kaen University (Mo Din Daeng)</li>
+            <li>Mr. Paitoon Narakorn | Assistant School Director of The demonstration school of Khon Kaen University (Mo Din Daeng)</li>
+            <li>Mr. Rawisut Junthawee | Project Consultant and Predecessor Webmaster</li>
+            </ul>
+            <div class="row">
+            <div class="col-12 col-md-6">
+            <h5 class="font-weight-bold">Beta Testers</h5>
+            <ul>
+            <li>Palapon Soontornpas</li>
+            <li>Sahatsawat Sudaphonrat</li>
+            <li>Sireethorn Pintaraks</li>
+            <li>Khwunjira Chunyakorn</li>
+            <li>Nutnitichai Srikonchan</li>
+            <li>Kritsakorn Wanjing</li>
+            <li>Sirikorn Kaewkot</li>
+            <li>Krist Sriwarom</li>
+            <li>Jirantanin Nualming</li>
+            <li>Kittitat Homnan</li>
+            <li>Supakit Konyai</li>
+            <li>Pakin Kraisin</li>
+            <li>Nontapan Sitthichotlertpakdee</li>
+            <li>Nonpawit Anantasipabut</li>
+            <li>Thanakrit Wong-asa</li>
+            <li>Yoswaris Lawpaiboon</li>
+            <li>Anon Jindawong</li>
+            <li>Mr. Narudon Phanomkam</li>
+            </ul>
+            </div>
+            <div class="col-12 col-md-6">
+            <h5 class="font-weight-bold">Special Thanks</h5>
+            <ul>
+            <li>Sireethorn Pintaraks</li>
+            <li>Peeranat Matsor</li>
+            <li>Yoswaris Lawpaiboon</li>
+            <li>Anon Jindawong</li>
+            <li>Pattarapol Jongkolrat</li>
+            <li>Thanakrit Wong-asa</li>
+            <li>Mr. Akkarawat Srisawat</li>
+            <li>Mr. Chedtharat Kongrat</li>
+            </ul>
+            </div>
+            </div>
+        </p>
+    </div>
+</body>
+
+<?php include '../global/footer.php' ?>
+<?php include '../global/popup.php' ?>
+
+</html>
