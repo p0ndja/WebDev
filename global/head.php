b275aa5245dba635e63157e90eb6e329c3d5b852
PondJaTH <palapon2545@gmail.com>
2020-01-27 18:18:06 +0700

    Update head.php

--- global/head.php
+++ global/head.php
@@ -1,43 +1,24 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<!-- KANIT FONT -->
-<link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
-
-<link rel="shortcut icon" href="../assets/images/logo/favicon.ico" type="image/x-icon">
-<link rel="icon" href="../assets/images/logo/favicon.ico" type="image/x-icon">
-<title>โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)</title>
-<link rel="image_src" href="../assets/images/logo/smdlogo.png" />
-<meta property="og:image" content="../assets/images/logo/smdlogo.png" />
-<meta property="og:title" content="โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)" />
-<meta property="og:description" content="123 มหาวิทยาลัยขอนแก่น โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ถนนมิตรภาพ ตำบลในเมือง อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40002 โทรศัพท์ / โทรสาร 043202044" />
-
-<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-
-<?php include 'config.php';
-if ($use_mdbootstrap) { ?>
-<link href="../assets/css/mdb.min.css" rel="stylesheet"> <!--MDBootstrap need to be higher than Bootstrap-->
-<?php }
-?>
-
-<link href="../assets/css/style.css" rel="stylesheet">
-
-<!-- Icon :: See More at https://fontawesome.com/icons?d=gallery -->
-<script src="https://kit.fontawesome.com/7454809de8.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-<!-- jQuery -->
-<script type="text/javascript" src="../assets/js/jquery.min.js"></script>
-<!-- Bootstrap tooltips -->
-<script type="text/javascript" src="../assets/js/popper.min.js"></script>
-<!-- Bootstrap core JavaScript -->
-<script type="text/javascript" src="../assets/js/bootstrap.min.js">
-</script>
-<!-- MDB core JavaScript -->
-<script type="text/javascript" src="../assets/js/mdb.min.js"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css" rel="stylesheet">
-<script src="../assets/js/summernote-bs4.js"></script>
-
-
-
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- KANIT FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="../assets/images/logo/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../assets/images/logo/favicon.ico" type="image/x-icon">
+    <title>โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)</title>
+    <link rel="image_src" href="../assets/images/logo/smdlogo.png" />
+    <meta property="og:image" content="../assets/images/logo/smdlogo.png" />
+    <meta property="og:title" content="โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)" />
+    <meta property="og:description" content="123 มหาวิทยาลัยขอนแก่น โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ถนนมิตรภาพ ตำบลในเมือง อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40002 โทรศัพท์ / โทรสาร 043202044" />
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <?php include 'config.php'; ?>
+    <?php if ($use_mdbootstrap) ?> <link href="../assets/css/mdb.min.css" rel="stylesheet">
+    <link href="../assets/css/style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/7454809de8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <script type="text/javascript" src="../assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="../assets/js/popper.min.js"></script>
+    <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../assets/js/mdb.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css" rel="stylesheet">
+    <script src="../assets/js/summernote-bs4.js"></script>
