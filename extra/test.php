6e2919c07f7374b5cfbecb3fee9fec52b5016feb
PondJaTH <palapon2545@gmail.com>
2020-02-26 13:54:09 +0700

    Update test.php
    test

--- extra/test.php
+++ extra/test.php
@@ -1,5 +1,23 @@
-<?php 
-    include '../global/connect.php';
+<?php include '../global/connect.php'; ?>
 
-    echo getConfig('global_snowEffect', 'bool', $conn);
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../global/head.php'; ?>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-normal fixed-top scrolling-navbar" id="nav"
+        role="navigation">
+        <?php include '../global/navbar.php'; ?>
+    </nav>
+    <div class="container" id="container" style="padding-top: 88px">
+        <script>swal("TEST");</script>
+    </div>
+</body>
+
+<?php include '../global/footer.php'; ?>
+<?php include '../global/popup.php'; ?>
+
+</html>
