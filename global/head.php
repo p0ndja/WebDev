9a4d138f87bb8c9a82066c69c9319b79430bb639
PondJaTH <palapon2545@gmail.com>
2020-02-26 13:54:06 +0700

    Update head.php
    add google icon & sweet alert support

--- global/head.php
+++ global/head.php
@@ -2,6 +2,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Athiti|Bai+Jamjuree|Chakra+Petch|Charm|Charmonman|Chonburi|Fahkwang|Itim|K2D|Kanit|KoHo|Kodchasan|Krub|Maitree|Mali|Mitr|Niramit|Pattaya|Pridi|Prompt|Sarabun|Sriracha|Srisakdi|Taviraj|Thasadith|Trirong&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="shortcut icon" href="../assets/images/logo/favicon.ico" type="image/x-icon">
     <link rel="icon" href="../assets/images/logo/favicon.ico" type="image/x-icon">
     <title>โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)</title>
@@ -11,13 +12,16 @@
     <meta property="og:description" content="123 มหาวิทยาลัยขอนแก่น โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ถนนมิตรภาพ ตำบลในเมือง อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40002 โทรศัพท์ / โทรสาร 043202044" />
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <?php include 'config.php'; ?>
-    <?php if ($use_mdbootstrap) ?> <link href="../assets/css/mdb.min.css" rel="stylesheet">
+    <?php if ($use_mdbootstrap) {?> <link href="../assets/css/mdb.min.css" rel="stylesheet"> <?php } ?>
     <link href="../assets/css/style.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/7454809de8.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <script type="text/javascript" src="../assets/js/jquery.min.js"></script>
     <script type="text/javascript" src="../assets/js/popper.min.js"></script>
     <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../assets/js/mdb.min.js"></script>
+    <?php if ($use_mdbootstrap) {?> <script type="text/javascript" src="../assets/js/mdb.min.js"></script> <?php } ?>
+    <!--Summernote-->
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote-bs4.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote-bs4.min.js"></script>
+    <!--SweetAlert-->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
