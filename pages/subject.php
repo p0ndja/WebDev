e13904b5feb97635b2a50537a0f15043c19ab21c
PondJaTH <palapon2545@gmail.com>
2020-03-03 14:07:36 +0700

    Moved folder

---
+++ pages/subject.php
@@ -0,0 +1,122 @@
+<?php include '../global/connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../global/head.php'; ?>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-normal fixed-top scrolling-navbar" id="nav"
+        role="navigation">
+        <?php include '../global/navbar.php'; ?>
+    </nav>
+    <div class="container" id="container" style="padding-top: 88px">
+        <div class="row">
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#maths">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Maths.jpg" class="img-fluid" alt="Maths" align="center">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#sci">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Science.jpg" class="img-fluid" alt="Science">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#foreign">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Foreign_Language.jpg" class="img-fluid"
+                            alt="Foreign Language">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#thai">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Thai.jpg" class="img-fluid" alt="Thai">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#social">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Social.jpg" class="img-fluid" alt="Social">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#pe">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/PE.jpg" class="img-fluid" alt="PE">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#tech">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Technology.jpg" class="img-fluid" alt="Technology">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#art">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Art.jpg" class="img-fluid" alt="Art">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#choose">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Chosen.jpg" class="img-fluid" alt="Up-to-Student">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-10 col-md-3 mb-3 mx-auto">
+                <a href="#club">
+                    <div class="view overlay zoom z-depth-1">
+                        <img src="../assets/images/subject/Club.jpg" class="img-fluid" alt="Club">
+                        <div class="mask flex-center rgba-black-slight">
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <hr>
+        <div id="maths">
+            <h3>TEST</h3>
+            <p>
+                PondJa
+            </p>
+        </div>
+    </div>
+</body>
+
+<?php include '../global/footer.php'; ?>
+<?php include '../global/popup.php'; ?>
+
+</html>
