3d23cc76d93f6300308e70a9f0fe6f8f664cc59e
PondJaTH <palapon2545@gmail.com>
2020-03-05 18:48:05 +0700

    Update admin/index.php
    Fixed style, Test style

--- admin/index.php
+++ admin/index.php
@@ -6,23 +6,29 @@
 <head>
     <?php include '../global/head.php';?>
     <style>
-    @media (min-width: 960px)
-        {
-        .card-columns {
-            -webkit-column-count: 3;
-            -moz-column-count: 3;
-            column-count: 3;
+        @media (min-width: 960px) {
+            .card-columns {
+                -webkit-column-count: 3;
+                -moz-column-count: 3;
+                column-count: 3;
+            }
         }
-    }
 
-    @media (max-width: 960px)
-        {
-        .card-columns {
-            -webkit-column-count: 2;
-            -moz-column-count: 2;
-            column-count: 2;
+        @media (max-width: 960px) {
+            .card-columns {
+                -webkit-column-count: 2;
+                -moz-column-count: 2;
+                column-count: 2;
+            }
         }
-    }
+
+        @media (max-width: 420px) {
+            .card-columns {
+                -webkit-column-count: 1;
+                -moz-column-count: 1;
+                column-count: 1;
+            }
+        }
     </style>
 </head>
 
@@ -31,176 +37,235 @@
         role="navigation">
         <?php include '../global/navbar.php'; ?>
     </nav>
-
     <?php needLogin(); ?>
-
     <div class="container-fluid" id="container" style="padding-top: 88px">
-    <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
-        <a class="btn-floating btn-lg green waves-effect waves-light" onclick="toastr.success('บันทึกค่าทั้งหมดเรียบร้อยแล้ว')"><i class="fas fa-save"></i></a>
-    </div>
-    <?php if(getConfig('global_override_checking_admin', 'bool', $conn)) { ?>
-    <div class="alert alert-warning" role="alert"><p>GLOBAL_OVERRIDE_CHECKING_ADMIN</p></div>
-    <?php } else needPermission('isAdmin', $conn);?>
+        <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+            <a class="btn-floating btn-lg green waves-effect waves-light"
+                onclick="toastr.success('บันทึกค่าทั้งหมดเรียบร้อยแล้ว')"><i class="fas fa-save"></i></a>
+        </div>
+        <?php if(getConfig('global_override_checking_admin', 'bool', $conn)) { ?>
+        <div class="alert alert-warning" role="alert">
+            <p>GLOBAL_OVERRIDE_CHECKING_ADMIN</p>
+        </div>
+        <?php } else needPermission('isAdmin', $conn);?>
+
+        <!-- Nav tabs -->
+<ul class="nav md-tabs nav-justified peach-gradient mx-0 mb-0 mt-1" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Global Variable</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">User</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">News</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Forum</a>
+  </li>
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content">
+  <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">AAAA</div>
+  <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">BBBB</div>
+  <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">CCCC</div>
+  <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">DDDDD</div>
+</div>
+
+
         <div class="card-columns">
             <div class="card mb-3">
                 <div class="card-body card-text mb-3">
-                <div class="card-title card-text"><h1>Global Variable</h1></div>
-                <hr>
-                <?php
+                    <div class="card-title card-text">
+                        <h1>Global Variable</h1>
+                    </div>
+                    <hr>
+                    <?php
                     $cor = mysqli_query($conn, "SELECT * FROM `config` WHERE title LIKE '%[Global]%'");
                     while($get_config = mysqli_fetch_array($cor, MYSQLI_ASSOC)) {
                         $b = $get_config['bool'];
                         if ($b == true) $b = ' checked';
                         else $b = ' ';
                 ?>
-                <!-- Material checked -->
-                <div class="switch switch-warning mb-1">
-                    <label>
-                        <input type="checkbox" name="<?php echo $get_config['name'];?>" <?php echo $b; ?>>
-                        <span class="lever"></span>
-                        <a style="color: black;" class="material-tooltip-main" data-toggle="tooltip" title="<?php echo $get_config['description'] . ' (' . $get_config['name'] . ')';?>">
-                        <?php echo $get_config['title'];?>
-                        </a>
-                    </label>
-                </div>
-                <?php if ($get_config['msg'] != null) { ?>
-                    <input type="text" id="<?php echo $get_config['name']; ?>" name="<?php echo $get_config['name']; ?>" class="form-control form-control-sm validate" <?php if (!$get_config['bool']) echo 'style="display: none"'; ?> value="<?php echo $get_config['msg'];?>" placeholder="<?php echo $get_config['msg_tooltip'];?>">
-                <?php } ?>
-                <hr>
-                <?php } ?>
+                    <!-- Material checked -->
+                    <div class="switch switch-warning mb-1">
+                        <label>
+                            <input type="checkbox" name="<?php echo $get_config['name'];?>" <?php echo $b; ?>>
+                            <span class="lever"></span>
+                            <a style="color: black;" class="material-tooltip-main" data-toggle="tooltip"
+                                title="<?php echo $get_config['description'] . ' (' . $get_config['name'] . ')';?>">
+                                <?php echo $get_config['title'];?>
+                            </a>
+                        </label>
+                    </div>
+                    <?php if ($get_config['msg'] != null) { ?>
+                    <input type="text" id="<?php echo $get_config['name']; ?>" name="<?php echo $get_config['name']; ?>"
+                        class="form-control form-control-sm validate"
+                        <?php if (!$get_config['bool']) echo 'style="display: none"'; ?>
+                        value="<?php echo $get_config['msg'];?>" placeholder="<?php echo $get_config['msg_tooltip'];?>">
+                    <?php } ?>
+                    <hr>
+                    <?php } ?>
                 </div>
             </div>
             <div class="card mb-3">
                 <div class="card-body card-text mb-3">
-                <div class="card-title card-text"><h1>Achievement Variable</h1></div>
-                <hr>
-                <?php
+                    <div class="card-title card-text">
+                        <h1>Achievement Variable</h1>
+                    </div>
+                    <hr>
+                    <?php
                     $cor = mysqli_query($conn, "SELECT * FROM `config` WHERE title LIKE '%[Achievement]%'");
                     while($get_config = mysqli_fetch_array($cor, MYSQLI_ASSOC)) {
                         $b = $get_config['bool'];
                         if ($b == true) $b = ' checked';
                         else $b = ' ';
                 ?>
-                <!-- Material checked -->
-                <div class="switch switch-warning mb-1">
-                    <label>
-                        <input type="checkbox" name="<?php echo $get_config['name'];?>" <?php echo $b; ?>>
-                        <span class="lever"></span>
-                        <a style="color: black;" class="material-tooltip-main" data-toggle="tooltip" title="<?php echo $get_config['description'] . ' (' . $get_config['name'] . ')';?>">
-                        <?php echo $get_config['title'];?>
-                        </a>
-                    </label>
-                </div>
-                <?php if ($get_config['msg'] != null) { ?>
-                    <input type="text" id="<?php echo $get_config['name']; ?>" name="<?php echo $get_config['name']; ?>" class="form-control form-control-sm validate" <?php if (!$get_config['bool']) echo 'style="display: none"'; ?> value="<?php echo $get_config['msg'];?>" placeholder="<?php echo $get_config['msg_tooltip'];?>">
-                <?php } ?>
-                <hr>
-                <?php } ?>
+                    <!-- Material checked -->
+                    <div class="switch switch-warning mb-1">
+                        <label>
+                            <input type="checkbox" name="<?php echo $get_config['name'];?>" <?php echo $b; ?>>
+                            <span class="lever"></span>
+                            <a style="color: black;" class="material-tooltip-main" data-toggle="tooltip"
+                                title="<?php echo $get_config['description'] . ' (' . $get_config['name'] . ')';?>">
+                                <?php echo $get_config['title'];?>
+                            </a>
+                        </label>
+                    </div>
+                    <?php if ($get_config['msg'] != null) { ?>
+                    <input type="text" id="<?php echo $get_config['name']; ?>" name="<?php echo $get_config['name']; ?>"
+                        class="form-control form-control-sm validate"
+                        <?php if (!$get_config['bool']) echo 'style="display: none"'; ?>
+                        value="<?php echo $get_config['msg'];?>" placeholder="<?php echo $get_config['msg_tooltip'];?>">
+                    <?php } ?>
+                    <hr>
+                    <?php } ?>
                 </div>
             </div>
             <div class="card mb-3">
                 <div class="card-body card-text mb-3">
-                <div class="card-title card-text"><h1>Home Variable</h1></div>
-                <hr>
-                <?php
+                    <div class="card-title card-text">
+                        <h1>Home Variable</h1>
+                    </div>
+                    <hr>
+                    <?php
                     $cor = mysqli_query($conn, "SELECT * FROM `config` WHERE title LIKE '%[Home]%'");
                     while($get_config = mysqli_fetch_array($cor, MYSQLI_ASSOC)) {
                         $b = $get_config['bool'];
                         if ($b == true) $b = ' checked';
                         else $b = ' ';
                 ?>
-                <!-- Material checked -->
-                <div class="switch switch-warning mb-1">
-                    <label>
-                        <input type="checkbox" name="<?php echo $get_config['name'];?>" <?php echo $b; ?>>
-                        <span class="lever"></span>
-                        <a style="color: black;" class="material-tooltip-main" data-toggle="tooltip" title="<?php echo $get_config['description'] . ' (' . $get_config['name'] . ')';?>">
-                        <?php echo $get_config['title'];?>
-                        </a>
-                    </label>
-                </div>
-                <?php if ($get_config['msg'] != null) { ?>
-                    <input type="text" id="<?php echo $get_config['name']; ?>" name="<?php echo $get_config['name']; ?>" class="form-control form-control-sm validate" <?php if (!$get_config['bool']) echo 'style="display: none"'; ?> value="<?php echo $get_config['msg'];?>" placeholder="<?php echo $get_config['msg_tooltip'];?>">
-                <?php } ?>
-                <hr>
-                <?php } ?>
+                    <!-- Material checked -->
+                    <div class="switch switch-warning mb-1">
+                        <label>
+                            <input type="checkbox" name="<?php echo $get_config['name'];?>" <?php echo $b; ?>>
+                            <span class="lever"></span>
+                            <a style="color: black;" class="material-tooltip-main" data-toggle="tooltip"
+                                title="<?php echo $get_config['description'] . ' (' . $get_config['name'] . ')';?>">
+                                <?php echo $get_config['title'];?>
+                            </a>
+                        </label>
+                    </div>
+                    <?php if ($get_config['msg'] != null) { ?>
+                    <input type="text" id="<?php echo $get_config['name']; ?>" name="<?php echo $get_config['name']; ?>"
+                        class="form-control form-control-sm validate"
+                        <?php if (!$get_config['bool']) echo 'style="display: none"'; ?>
+                        value="<?php echo $get_config['msg'];?>" placeholder="<?php echo $get_config['msg_tooltip'];?>">
+                    <?php } ?>
+                    <hr>
+                    <?php } ?>
                 </div>
             </div>
             <div class="card mb-3">
                 <div class="card-body card-text mb-3">
-                <div class="card-title card-text"><h1>User Variable</h1></div>
-                <hr>
-                <?php
+                    <div class="card-title card-text">
+                        <h1>User Variable</h1>
+                    </div>
+                    <hr>
+                    <?php
                     $cor = mysqli_query($conn, "SELECT * FROM `config` WHERE title LIKE '%[User]%'");
                     while($get_config = mysqli_fetch_array($cor, MYSQLI_ASSOC)) {
                         $b = $get_config['bool'];
                         if ($b == true) $b = ' checked';
                         else $b = ' ';
                 ?>
-                <!-- Material checked -->
-                <div class="switch switch-warning mb-1">
-                    <label>
-                        <input type="checkbox" name="<?php echo $get_config['name'];?>" <?php echo $b; ?>>
-                        <span class="lever"></span>
-                        <a style="color: black;" class="material-tooltip-main" data-toggle="tooltip" title="<?php echo $get_config['description'] . ' (' . $get_config['name'] . ')';?>">
-                        <?php echo $get_config['title'];?>
-                        </a>
-                    </label>
-                </div>
-                <?php if ($get_config['msg'] != null) { ?>
-                    <input type="text" id="<?php echo $get_config['name']; ?>" name="<?php echo $get_config['name']; ?>" class="form-control form-control-sm validate" <?php if (!$get_config['bool']) echo 'style="display: none"'; ?> value="<?php echo $get_config['msg'];?>" placeholder="<?php echo $get_config['msg_tooltip'];?>">
-                <?php } ?>
-                <hr>
-                <?php } ?>
+                    <!-- Material checked -->
+                    <div class="switch switch-warning mb-1">
+                        <label>
+                            <input type="checkbox" name="<?php echo $get_config['name'];?>" <?php echo $b; ?>>
+                            <span class="lever"></span>
+                            <a style="color: black;" class="material-tooltip-main" data-toggle="tooltip"
+                                title="<?php echo $get_config['description'] . ' (' . $get_config['name'] . ')';?>">
+                                <?php echo $get_config['title'];?>
+                            </a>
+                        </label>
+                    </div>
+                    <?php if ($get_config['msg'] != null) { ?>
+                    <input type="text" id="<?php echo $get_config['name']; ?>" name="<?php echo $get_config['name']; ?>"
+                        class="form-control form-control-sm validate"
+                        <?php if (!$get_config['bool']) echo 'style="display: none"'; ?>
+                        value="<?php echo $get_config['msg'];?>" placeholder="<?php echo $get_config['msg_tooltip'];?>">
+                    <?php } ?>
+                    <hr>
+                    <?php } ?>
                 </div>
             </div>
         </div>
     </div>
-<script>
-    $('.form-control').on('change', function(e) {
-    var x = document.getElementById(e.target.name).value;
-    console.log("ID: " + e.target.name + " -> " + x);
-    $.ajax({
-        url: "save.php",  
-        type: "POST",
-        //pass data like this 
-        data: {name: e.target.name, col: "msg", val: x},
-        cache: false,
-        success: function(data) {  
-        if (data=="S")
-            $('#message').html("<h2>Current balance has been updated!</h2>") 
-        } 
-    });
-    toastr.success("อัพเดทค่า '" + e.target.name + "' เป็น " + x);
-    });
+    <script>
+        $('.form-control').on('change', function (e) {
+            var x = document.getElementById(e.target.name).value;
+            console.log("ID: " + e.target.name + " -> " + x);
+            $.ajax({
+                url: "save.php",
+                type: "POST",
+                //pass data like this 
+                data: {
+                    name: e.target.name,
+                    col: "msg",
+                    val: x
+                },
+                cache: false,
+                success: function (data) {
+                    if (data == "S")
+                        $('#message').html("<h2>Current balance has been updated!</h2>")
+                }
+            });
+            toastr.success("อัพเดทค่า '" + e.target.name + "' เป็น " + x);
+        });
 
-    $('.switch input[type="checkbox"]').on('change', function(e) {
-    var x = document.getElementById(e.target.name);
-    if (x != null) {
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }
-    }
+        $('.switch input[type="checkbox"]').on('change', function (e) {
+            var x = document.getElementById(e.target.name);
+            if (x != null) {
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+            }
 
-    console.log(e.target.checked);
-    console.log(e.target.name);
-    $.ajax({
-        url: "save.php",  
-        type: "POST",
-          //pass data like this 
-          data: {name: e.target.name, col: "bool", val: e.target.checked},
-        cache: false,
-        success: function(data) {  
-            if (data=="S")
-                $('#message').html("<h2>Current balance has been updated!</h2>") 
-        } 
-    });
+            console.log(e.target.checked);
+            console.log(e.target.name);
+            $.ajax({
+                url: "save.php",
+                type: "POST",
+                //pass data like this 
+                data: {
+                    name: e.target.name,
+                    col: "bool",
+                    val: e.target.checked
+                },
+                cache: false,
+                success: function (data) {
+                    if (data == "S")
+                        $('#message').html("<h2>Current balance has been updated!</h2>")
+                }
+            });
 
-    toastr.success("อัพเดทค่า '" + e.target.name + "' เป็น " + e.target.checked);
-    });
-</script>
+            toastr.success("อัพเดทค่า '" + e.target.name + "' เป็น " + e.target.checked);
+        });
+    </script>
 </body>
 
 <?php include '../global/footer.php' ?>
