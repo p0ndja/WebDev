f6978afefc15ed3f834ca8d164a381155954eba4
PondJaTH <palapon2545@gmail.com>
2020-03-06 21:33:11 +0700

    Update save.php
    Update to match with function.php

--- admin/save.php
+++ admin/save.php
@@ -1,11 +1,9 @@
 <?php
     include '../global/connect.php';
     if (isset($_POST['name']) && isset($_POST['col']) && isset($_POST['val'])) {
-        $q = "UPDATE `config` SET " . $_POST['col'] . " = ". $_POST['val'] . " WHERE name = '" . $_POST['name'] . "'";
-        echo $q;
-        $r = mysqli_query($conn, $q);
-        if (!$r)
-            die('Could not get data: '.mysqli_error($conn));
-        else echo "S";
+        return saveConfig($_POST['name'], $_POST['col'], '"'.$_POST['val'].'"', $conn);
+    }
+    if (isset($_POST['name']) && isset($_POST['col']) && isset($_POST['bool'])) {
+        return saveConfig($_POST['name'], $_POST['col'], $_POST['bool'], $conn);
     }
 ?>
