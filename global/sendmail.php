397b4b927b99a2753283cd5f0755ae3e4ec8f7bc
PondJaTH <palapon2545@gmail.com>
2020-03-06 21:33:00 +0700

    Update sendmail.php

--- global/sendmail.php
+++ global/sendmail.php
@@ -3,7 +3,7 @@
 $emailto='palapon2545@gmail.com'; //อีเมล์ผู้รับ
 $subject="ทดสอบระบบ"; //หัวข้อ
 $header.="Content-type: text/html; charset=utf-8\n";
-$header.="from: ทดสอบ E-mail : test@pondja.com"; //ชื่อและอีเมลผู้ส่ง
+$header.="from: ทดสอบ E-mail : pondjaco@pondja.com"; //ชื่อและอีเมลผู้ส่ง
 $messages.= "TEXT"
 $send_mail=mail($emailto, $subject, $messages, $header);
 
