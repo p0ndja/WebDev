41c098a59c698fd652b0be917262f27cf841f5d6
PondJaTH <palapon2545@gmail.com>
2020-03-29 21:18:56 +0700

    Update function.php
    add getClientIP

--- global/function.php
+++ global/function.php
@@ -66,6 +66,14 @@
 ?>
 
 <?php
+    function getClientIP() {
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])) return $_SERVER['HTTP_CLIENT_IP'];
+        else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        return $_SERVER['REMOTE_ADDR'];
+    }
+?>
+
+<?php
 
     function base64($f, $user, $type) {
         $name=$f['name'];
