68e474e28db9870b3462f3b76bb85483b629c335
PondJaTH <palapon2545@gmail.com>
2020-04-13 17:55:58 +0700

    update assets

--- global/head.php
+++ global/head.php
@@ -10,20 +10,20 @@
     <meta property="og:image" content="../assets/images/default/thumbnail.jpg" />
     <meta property="og:title" content="โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ฝ่ายมัธยมศึกษา (มอดินแดง)" />
     <meta property="og:description" content="123 มหาวิทยาลัยขอนแก่น โรงเรียนสาธิตมหาวิทยาลัยขอนแก่น ถนนมิตรภาพ ตำบลในเมือง อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40002 โทรศัพท์ / โทรสาร 043202044" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/bootstrap/bootstrap.min.css">
     <?php include 'config.php'; ?>
     <?php if ($use_mdbootstrap) {?> <link href="../assets/css/mdb.min.css" rel="stylesheet"> <?php } ?>
-    <!--link href="../assets/css/fonts.css" rel="stylesheet"-->
     <link href="../assets/css/style.css" rel="stylesheet">
+    <link href="../assets/css/fonts.css" rel="stylesheet">
     <link href="../assets/css/dark-mode.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/7454809de8.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="../assets/js/3.4.1-jquery.min.js"></script>
+    <script src="../assets/js/1.16.0-popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="../assets/bootstrap/bootstrap.min.js"></script>
     <?php if ($use_mdbootstrap) {?> <script type="text/javascript" src="../assets/js/mdb.min.js"></script> <?php } ?>
     <!--Summernote-->
     <link href="../assets/summernote/summernote-bs4.min.css" rel="stylesheet">
     <script src="../assets/summernote/summernote-bs4.min.js"></script>
     <!--SweetAlert-->
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="../assets/js/sweetalert.min.js"></script>
