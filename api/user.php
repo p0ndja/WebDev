54dd9f964a5b7cbdc2ad221b8fe95ec663640c7d
PondJaTH <palapon2545@gmail.com>
2020-04-20 22:58:24 +0700

    Update user.php

--- api/user.php
+++ api/user.php
@@ -1,28 +1,25 @@
 <?php
-include '../global/connect.php';
+    include '../global/connect.php';
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
 
-// required headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+    $q = "SELECT * FROM `user`";
+    $r = mysqli_query($conn, $q);
 
-$q = "SELECT * FROM `user`";
-$r = mysqli_query($conn, $q);
+    $arr = array();
+    $arr["user"] = array();
 
-    $products_arr = array();
-    $products_arr["user"] = array();
+    while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+        
+        $item=array(
+            "id" => $row['id'],
+            "user" => $row['username'],
+            "grade" => $row['grade'],
+            "class" => $row['class']
+        );
 
-while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-    $product_item=array(
-        "id" => $row['id'],
-        "user" => $row['username'],
-        "grade" => $row['grade'],
-        "class" => $row['class']
-    );
+        array_push($arr["user"], $item);
+    }
 
-    array_push($products_arr["user"], $product_item);
-}
-echo json_encode($products_arr);
-
-  
-// database connection will be here
+    echo json_encode($arr);
 ?>
