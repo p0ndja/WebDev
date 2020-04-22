3f08d04f9d53deeb3090e364e33724b7d4f8611a
PondJaTH <palapon2545@gmail.com>
2020-04-22 13:56:51 +0700

    Update student.php
    change dictionary

--- api/student.php
+++ api/student.php
@@ -11,15 +11,18 @@
 
     while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
         
+        $arr["std"][$row['id']] = array();
+
         $item=array(
             "id" => $row['id'],
+            "prefix" => $row['prefix'],
             "firstname" => $row['firstname'],
             "lastname" => $row['lastname'],
             "grade" => $row['grade'],
             "class" => $row['class']
         );
 
-        array_push($arr["std"], $item);
+        array_push($arr["std"][$row['id']], $item);
     }
 
     echo json_encode($arr, JSON_UNESCAPED_UNICODE);
