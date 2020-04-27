cb714409d5df186387da95780c1159542f0e9607
PondJaTH <palapon2545@gmail.com>
2020-04-27 23:07:13 +0700

    Update config.php
    remove mdbootstrap choice

--- global/config.php
+++ global/config.php
@@ -1,5 +1,4 @@
 <?php
-    $use_mdbootstrap = true;
     $snow_effect = false; //If enable this, Script for loading modal will bug.
     $patch = "ปรับปรุงล่าสุด: 25 มกราคม 2020";
     $close = false;
