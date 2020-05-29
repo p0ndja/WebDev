a1f136640eba72e4e49098ba8640513da151278a
PondJaᵀᴴ <palapon2545@gmail.com>
2020-05-29 13:39:17 +0700

    api update

--- global/api/student.php
+++ global/api/student.php
@@ -1,24 +1,28 @@
 <?php
-    require '../global/connect.php';
+    require '../../global/connect.php';
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
 
-    $q = "SELECT * FROM `user`";
+    $q = "SELECT * FROM `std_2563`";
     $r = mysqli_query($conn, $q);
 
     $arr = array();
-    $arr["user"] = array();
+    $arr["std"] = array();
 
     while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
         
+        $arr["std"][$row['id']] = array();
+
         $item=array(
             "id" => $row['id'],
-            "user" => $row['username'],
+            "prefix" => $row['prefix'],
+            "firstname" => $row['firstname'],
+            "lastname" => $row['lastname'],
             "grade" => $row['grade'],
             "class" => $row['class']
         );
 
-        array_push($arr["user"], $item);
+        array_push($arr["std"][$row['id']], $item);
     }
 
     echo json_encode($arr, JSON_UNESCAPED_UNICODE);
--- global/api/user.php
+++ global/api/user.php
@@ -1,28 +1,24 @@
 <?php
-    require '../global/connect.php';
+    require '../../global/connect.php';
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
 
-    $q = "SELECT * FROM `std_2563`";
+    $q = "SELECT * FROM `user`";
     $r = mysqli_query($conn, $q);
 
     $arr = array();
-    $arr["std"] = array();
+    $arr["user"] = array();
 
     while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
         
-        $arr["std"][$row['id']] = array();
-
         $item=array(
             "id" => $row['id'],
-            "prefix" => $row['prefix'],
-            "firstname" => $row['firstname'],
-            "lastname" => $row['lastname'],
+            "user" => $row['username'],
             "grade" => $row['grade'],
             "class" => $row['class']
         );
 
-        array_push($arr["std"][$row['id']], $item);
+        array_push($arr["user"], $item);
     }
 
     echo json_encode($arr, JSON_UNESCAPED_UNICODE);
