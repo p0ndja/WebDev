58ab34ff83543a043e53faae531b0bea49bc3e07
PondJaᵀᴴ <palapon2545@gmail.com>
2020-06-12 00:12:30 +0700

    Update profile/save.php
    Use upload real file instead of base64

--- profile/save.php
+++ profile/save.php
@@ -13,18 +13,49 @@
         $re = mysqli_query($conn, "UPDATE `profile` set greetings = '$text' WHERE id = $id");
         if (! $re) die('Could not update text: ' . mysqli_error($conn));
         
+
             
         if(isset($_FILES['profile_upload']) && $_FILES['profile_upload']['name'] != ""){
-            $base64 = base64($_FILES['profile_upload'], $user, 'image');
-            $r = mysqli_query($conn, "UPDATE `profile` SET profile = '$base64' WHERE id = $id");
-            if (! $r) die("Could not set profile: " . mysqli_error($conn));
+            if ($_FILES['profile_upload']['name']) {
+                if (!$_FILES['profile_upload']['error']) {
+                    $name = "Avatar";
+                    $ext = explode('.', $_FILES['profile_upload']['name']);
+                    $filename = $name . '.' . $ext[1];
+        
+                    if (!file_exists('../file/profile/'. $id .'/')) {
+                        mkdir('../file/profile/'. $id .'/');
+                    }
+        
+                    $destination = '../file/profile/'. $id .'/' . $filename; //change this directory
+                    $location = $_FILES["profile_upload"]["tmp_name"];
+                    move_uploaded_file($location, $destination);
+                    $finalFile = '../file/profile/'. $id .'/' . $filename;//change this URL
+                    $r = mysqli_query($conn, "UPDATE `profile` SET profile = '$finalFile' WHERE id = $id");
+                    if (! $r) die("Could not set profile: " . mysqli_error($conn));
+                }
+            }
         }
 
         if(isset($_FILES['background_upload']) && $_FILES['background_upload']['name'] != ""){
-            $base64 = base64($_FILES['background_upload'], $user, 'image');
-            $isDark = $_POST['isDark'];
-            $r = mysqli_query($conn, "UPDATE `profile` SET background = '$base64', isDark = $isDark WHERE id = $id");
-            if (! $r) die("Could not set profile_background: " . mysqli_error($conn));
+            if ($_FILES['background_upload']['name']) {
+                if (!$_FILES['background_upload']['error']) {
+                    $name = "Background";
+                    $ext = explode('.', $_FILES['background_upload']['name']);
+                    $filename = $name . '.' . $ext[1];
+        
+                    if (!file_exists('../file/profile/'. $id .'/')) {
+                        mkdir('../file/profile/'. $id .'/');
+                    }
+        
+                    $destination = '../file/profile/'. $id .'/' . $filename; //change this directory
+                    $location = $_FILES["background_upload"]["tmp_name"];
+                    move_uploaded_file($location, $destination);
+                    $finalFile = '../file/profile/'. $id .'/' . $filename;//change this URL
+                    $isDark = $_POST['isDark'];
+                    $r = mysqli_query($conn, "UPDATE `profile` SET background = '$finalFile', isDark = $isDark WHERE id = $id");
+                    if (! $r) die("Could not set profile: " . mysqli_error($conn));
+                }
+            }
         }
     }
     header("Location: ../profile");
