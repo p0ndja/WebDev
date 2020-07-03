5dd0670291c0b1459c8ba2cd81b227422c860d30
PondJaᵀᴴ <palapon2545@gmail.com>
2020-07-04 01:02:19 +0700

    *
    Update Livestream Page [test]

--- pages/live.php
+++ pages/live.php
@@ -6,24 +6,22 @@
 <head>
     <?php require '../global/head.php'; ?>
     <style>
-    .fix{
-    position:fixed;
-    bottom:0px;
-    left:0%;
-    width:100%;
-}
-body{background-color:#000000 !important;
-  background-image: url('https://cdn.discordapp.com/attachments/636478500206936094/702438313549758514/movie-theater-04.jpg');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: bottom center;}
-[data-theme="dark"] body {
-    background-color: #000000 !important;
-  background-image: url('https://cdn.discordapp.com/attachments/636478500206936094/702438313549758514/movie-theater-04.jpg');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: bottom center;
-}
+        .fix {
+            position:fixed;
+            bottom: 0px;
+            left: 0%;
+            width: 100%;
+        }
+        body {
+            background-color:#000000 !important;
+            background-image: url('https://cdn.discordapp.com/attachments/636478500206936094/702438313549758514/movie-theater-04.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: bottom center;
+        }
+        [data-theme="dark"] body {
+            background-color: #000000 !important;
+        }
     </style>
 </head>
 
@@ -33,14 +31,11 @@ body{background-color:#000000 !important;
         <?php require '../global/navbar.php'; ?>
     </nav>
     <div class="container" id="container" style="padding-top: 88px">
-    <div class="center"><div class="card mb-3">
-                    <div class="card-body">
-                        <div class="container justify-content-center">
-                            Lorem
-                        </div>
-                    </div>
-                </div>
-    </div>
+        <div class="card mb-3">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/eKJa2K7ehBo" allowfullscreen></iframe>
+            </div>
+        </div>
     </div>
 <?php require '../global/popup.php'; ?>
 <div class="d-none">
