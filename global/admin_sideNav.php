956a4f8daa4723a764f3408c7e85a05d447251cd
PondJaᵀᴴ <palapon2545@gmail.com>
2020-07-14 22:52:46 +0700

    [Removed]

--- global/admin_sideNav.php
+++
@@ -1,7 +0,0 @@
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
-</div>
