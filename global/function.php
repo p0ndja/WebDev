8db8cde5615910032f694f4b166ecb571a804ff5
PondJaTH <palapon2545@gmail.com>
2020-07-16 01:23:58 +0700

    ⚙ Technical Update
    Support using alternative image server

--- global/function.php
+++ global/function.php
@@ -359,6 +359,33 @@
         else return false;
     }
 
+    function sendFileToIMGHost($file) {
+        $data = array(
+            'img' => new CURLFile($file['tmp_name'],$file['type'], $file['name']),
+        ); 
+        
+        //**Note :CURLFile class will work if you have PHP version >= 5**
+        
+         $ch = curl_init();
+        
+        curl_setopt($ch, CURLOPT_URL, 'https://img.p0nd.ga/upload.php');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 86400); // 1 Day Timeout
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60000);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_REFERER, $_SERVER['HTTP_HOST']);
+        
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+            $msg = FALSE;
+        } else {
+            $msg = $response;
+        }
+        
+        curl_close($ch);
+        return $msg;
+    }
+
 
     function generateOpenGraphMeta($conn) {
         $current_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
