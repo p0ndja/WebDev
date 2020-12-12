ba146f15f1fc8a0336ceeb446754f713eb6afb05
PondJaᵀᴴ <palapon2545@gmail.com>
2020-12-12 21:37:04 +0700

    *

--- pages/flipbook_loader.php
+++ pages/flipbook_loader.php
@@ -9,7 +9,7 @@
 
 <body>
     <div class="container" id="container">
-        <div class="_df_book" webgl="true" backgroundcolor="transparent" source="<?php echo $_GET['file']?>" id="df_manual_book"></div>
+        <div class="_df_book" webgl="true" backgroundcolor="transparent" source="<?php echo $_GET['file']?>" id="df_manual_book" height="1080" weight="1920"></div>
     </div>
     <div class="d-none">
 <?php require '../global/popup.php'; ?>
